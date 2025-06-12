1314ff5303776f7e08d0db046799d066bcfce5f4
Vishnu Deb Jha <jhavishnu100@gmail.com>
2025-06-12 20:23:52 +0530

    dashboard section ongoing. Info Card, Dashboard layout, Navbar, Sidemenu, data.js, helper.js, Dashboard done

--- frontend/src/pages/Admin/Dashboard.jsx
+++ frontend/src/pages/Admin/Dashboard.jsx
@@ -1,12 +1,60 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useUserAuth } from '../../hooks/useUserAuth'
+import { useContext } from 'react'
+import { UserContext } from '../../context/UserContext'
+import DashboardLayout from '../../components/layouts/DashboardLayout'
+import { useNavigate } from 'react-router-dom'
+import axiosInstance from '../../utils/axiosInstance'
+import { API_PATH } from '../../utils/apiPaths'
+import moment from 'moment'
+import { addThousandSeprators } from '../../utils/helper'
+import InfoCard from '../../components/cards/InfoCard'
 
 const Dashboard = () => {
   useUserAuth()
+  const { user } = useContext(UserContext)
+  const navigate = useNavigate()
+
+  const [dashboardData, setDashboardData] = useState(null)
+  const [pieChartData, setPieChartData] = useState([])
+  const [barChartData, setBarChartData] = useState([])
+
+  const getDashboardData = async () => {
+    try {
+      const response = await axiosInstance.get(API_PATH.TASKS.GET_DASHBOARD_DATA)
+      if(response.data){
+        setDashboardData(response.data)
+      }
+    } catch (error) {
+      console.error("Error fetching users:", error)
+    }
+  }
+
+  useEffect(() => {
+    getDashboardData()
+
+    return () => {}
+  }, [])
+
   return (
-    <div>
-      Hello
-    </div>
+    <DashboardLayout activeMenu="Dashboard">
+      <div className='card my-5'>
+        <div>
+          <div className='col-span-3'>
+            <h2 className='text-xl md:text-2xl'>Good Morning! {user?.name}</h2>
+            <p className='text-xs md:text-[13px] text-gray-400 mt-1.5'>
+              {moment().format("dddd Do MMM YYYY")}
+            </p>
+          </div>
+        </div>
+        <div className='grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 gap-3 md:gap-6 mt-5'>
+           <InfoCard label="Total tasks" value={addThousandSeprators( dashboardData?.charts?.taskDistribution?.All || 0)} color="bg-primary" />
+           <InfoCard label="Pending tasks" value={addThousandSeprators( dashboardData?.charts?.taskDistribution?.Pending || 0)} color="bg-violet-500" />
+           <InfoCard label="In Progress tasks" value={addThousandSeprators( dashboardData?.charts?.taskDistribution?.InProgress || 0)} color="bg-cyan-500" />
+           <InfoCard label="Completed tasks" value={addThousandSeprators( dashboardData?.charts?.taskDistribution?.Completed || 0)} color="bg-lime-500" />
+        </div>
+      </div>
+    </DashboardLayout>
   )
 }
 
